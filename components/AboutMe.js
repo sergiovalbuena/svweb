d4b1cdf4c1312554edbb4dda189fa52dbaccc22e
srji <iosergio.valbuena@gmail.com>
2022-08-02 18:00:50 -0700

    Beta aboutme done

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -5,10 +5,10 @@ export default function AboutMe() {
     <div className="Wrapper">
       <h2>Something about me</h2>
     
-      <p>I'm a  from 🇨🇴, living in 🇨🇦, but feeling just a human being in such a wonderful planet 🌎.</p>
-      <p>I studied Design at <a href="https://uniandes.edu.co/en">Andes Univeristy</a> then I get some professional experiences in Digital Marketing and product Design. Had the great chance to work for Sodexo, Renault, Disney, also as a Freelancer and as a Product Consultan. Now I'm a Interactive Media Developer student at <a href="https://camosun.ca/">Camosun College</a> .</p>
+      <p>I&#39;m yogui 🧘🏻‍♂️, traveler 🛩, designer and developer from <span>🇨🇴 </span> , living in <span>🇨🇦</span> , but feeling just a borderless human being in such a wonderful planet <span>🌎</span>. </p>
+      <p>I <span>🎓</span>  studied Design at <a href="https://uniandes.edu.co/en">  Andes Univeristy</a> then I get some professional experiences <span>👷🏽 </span>  in Digital Marketing and product Design. Had the great chance to work for Sodexo, Renault, Disney. I have expereince, also, as a Freelancer and as a Product Design Consultan. Now I&#39;m a Interactive Media Developer student <span>👨🏽‍🎓</span> at <a href="https://camosun.ca/">Camosun College</a> .</p>
       {/* <p>For me is so important to do the best, also giving the extra mile, to achive that I do meditation and yoga every morning 🧘🏻‍♂️ , to have my brain 🧠  and  soul in sharped.</p> */}
-      <p>I Love coding because it is my favorite way to create solutions and great experiences at same time. </p>
+      <p>I love <span>❣️</span>  coding because it&#39;s my favorite way to create solutions and great experiences at same time. </p>
    
       <div className="redes">
         
@@ -16,6 +16,9 @@ export default function AboutMe() {
    
    
       <style jsx>{`
+
+@import url('https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap');
+
       .Wrapper{
         width: 100%;
         margin: 20px;
@@ -23,6 +26,17 @@ export default function AboutMe() {
       }
       .Wrapper p{
         padding:  0 30px 0 10px;
+        font-family: 'Nanum Pen Script', cursive;
+        font-size: 2rem
+      }
+
+      .Wrapper p a{
+        font-family: 'Nanum Pen Script', cursive;
+        font-size: 2rem;
+        text-decoration: underline;
+      }
+      span{
+        font-size:2rem;
       }
       
       `}</style>
